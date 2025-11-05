7ff308dd8b04badf269f70e441e6b8f12edaa3f0
Eltoon404 <elton.sadiku@internship.wayland.io>
2025-11-05 08:38:20 +0100

    Removing comments in main and fixing ymal file

--- main.cpp
+++ main.cpp
@@ -3,7 +3,6 @@
 int main() {
   auto app = holoscan::make_application<V4L2OverlayApp>();
   app->name("Camera Holoviz Example");
-  // Load operator configuration (device, sizes, holoviz window, etc.)
   app->config("config/cam.yaml");
   app->run();
   return 0;
